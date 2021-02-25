0238e0e8468baf79e19507288f5fa20a4f141256
Valeria Achoa <73545138+achoav@users.noreply.github.com>
2021-02-24 22:41:35 -0500

    update

--- Simple_Map/Static/js/logic.js
+++ Simple_Map/Static/js/logic.js
@@ -2,7 +2,12 @@
 console.log("working");
 
 //  Create the map object with options.
-let map = L.map('mapid').setView([40.7, -94.5],4);
+let map = L.map('mapid').setView([34.0522, -118.2437],14);
+
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Add a circle to the map on the locatiom, black outlined, filled with yellow color
+L.circleMarker([34.0522, -118.2437], {radius: 300, color: "black", fillColor: '#ffffa1'}).addTo(map); 
 
 // We create the tile layer for "STREETS" that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -10,5 +15,5 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 	maxZoom: 18,
 	accessToken: API_KEY
 });
-
+// We create the tile layer that will be the background of our map.
 streets.addTo(map)
